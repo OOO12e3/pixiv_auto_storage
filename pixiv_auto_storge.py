5ea5e6d3b5497e2dc30c6749473ddc9526eab99a
OOO12e3 <mcbox12e3@outlook.com>
2025-09-06 11:07:12 +0800

    change txt to epub

--- pixiv_auto_storge.py
+++ pixiv_auto_storge.py
@@ -8,9 +8,13 @@ import requests
 import os
 import re
 import sqlite3
+from ebooklib import epub
 
-if not os.path.exists("./novel"):
-        os.makedirs("./novel")
+if not os.path.exists("novel"):
+        os.makedirs("novel")
+
+if not os.path.exists("img"):
+    os.makedirs("img")
 
 try:
     with open("script_config.json","r",encoding='utf-8') as f:          #导入用户网站
@@ -18,17 +22,24 @@ try:
 except:
     with open("script_config.json","w",encoding="utf-8") as f:
         data = {
-        "website": {
+    "website": {
         "url": "",
         "comment": "user's pixiv page"
     },
-    "user_id":{
-    "id":"",
-    "comment":"enter your pixiv id"
+    "user_id": {
+        "id": "",
+        "comment": "enter your pixiv id"
     },
-        "header": {
-            "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36"
-            }
+    "header": {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36 Edg/139.0.0.0"
+    },
+    "img_header": {
+        "referer":"https://www.pixiv.net",
+        "sec-ch-ua":"\"Not;A=Brand\";v=\"99\", \"Microsoft Edge\";v=\"139\", \"Chromium\";v=\"139\"",
+        "sec-ch-ua-mobile":"?0",
+        "sec-ch-ua-platform":"\"Linux\"",
+        "user-agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36 Edg/139.0.0.0"
+    }
 }
         json.dump(data, f, ensure_ascii=False, indent=4)
 
@@ -62,6 +73,7 @@ except:
 url = config["website"]["url"]
 header = config["header"]
 cookie_dict = {item["name"]: item["value"] for item in cookie}
+img_header = config["img_header"]
 
 response = requests.get("https://www.pixiv.net/ajax/user/"+config["user_id"]["id"]+"/novels/bookmarks?tag=&offset=0&limit=30&rest=show&lang=zh", cookies=cookie_dict, headers=header, timeout=5)
 
@@ -95,6 +107,7 @@ for i in range(page_limit):
             links[work_title] = "https://www.pixiv.net/ajax/novel/"+work_id+"?lang=zh"
     time.sleep(3)
 
+'''
 for title, api_link in links.items():
     response = requests.get(api_link, cookies=cookie_dict, headers=header)
     print("[{:^10}] require novel {} |statue: {:^3}|".format(time.time(),title,response.status_code))
@@ -118,7 +131,70 @@ for title, api_link in links.items():
             f.write("\ncontent: \n\n")
             f.write(content_text)
     time.sleep(3)
+'''
 
+def download_img(url,id,cookie,header):
+    try:
+        res = requests.get(url,stream=True,cookies=cookie,headers=header)
+        res.raise_for_status()
+        with open("img/{}.jpg".format(id),"wb") as f:
+            for chunk in res.iter_content(chunk_size=8192):
+                f.write(chunk)
+    except:
+        print("download failed")
+
+def change_to_html(json_content):
+    if not json_content:  # 处理 None 或空字符串
+        return "<p>none</p>"
+    html_content = json_content
+    html_content = re.sub(r'\n{3,}', '</p><p>', html_content)
+    html_content = re.sub(r'\n{2}', '</p><p>', html_content)
+    html_content = html_content.replace('\n', '<br/>')
+    html_content= re.sub("\[uploadedimage:(\w+)\]",r"<img src=\"images/\1.jpg\">",html_content)
+    return html_content
+
+for title, api_link in links.items():
+    response = requests.get(api_link, cookies=cookie_dict, headers=header)
+    print("[{:^10}] require novel {} |statue: {:^3}|".format(time.time(),title,response.status_code))
+    data = response.json()
+    if not data["error"]:
+        book = epub.EpubBook()
+        title = data["body"]["title"]
+        description = data["body"]["description"]
+        content_html = data["body"]["content"]
+        author = data["body"]["userName"]
+        book.set_title(title)
+        book.add_author(author)
+
+        
+        images_if = data["body"].get("textEmbeddedImages")
+        if images_if:  # 只有在有图片时才执行
+            for img_key, img_value in data["body"]["textEmbeddedImages"].items():
+                download_img(img_value["urls"]["original"],img_key,cookie=cookie_dict,header=img_header)
+                with open("img/{}.jpg".format(img_key),"rb") as f:
+                    img_tmp = epub.EpubImage(uid=img_key, file_name='images/'+img_key+".jpg", media_type='image/jpeg', content=f.read())
+                    book.add_item(img_tmp)
+        
+        book_description = epub.EpubHtml(title='description', file_name='description.xhtml', lang='zh')
+        book_description.content = change_to_html(description)
+        book.add_item(book_description)
+
+        book_content = epub.EpubHtml(title="content",file_name="content.xhtml",lang="zh")
+        book_content.content = change_to_html(content_html)
+        book.add_item(book_content)
+
+        img_key = "cover"
+        download_img(data["body"]["coverUrl"],img_key,cookie=cookie_dict,header=img_header)
+        with open("img/cover.jpg","rb") as f:
+            book.set_cover("cover.jpg", f.read())
+
+        
+        book.spine = [book_description, book_content]
+        epub.write_epub("novel/"+title+".epub",book,{})
+    
+        
+    time.sleep(3)
+    
 '''
 with open("storge.json","w",encoding="utf-8") as f:
     json.dump(storge,f,ensure_ascii=False,indent=4)
